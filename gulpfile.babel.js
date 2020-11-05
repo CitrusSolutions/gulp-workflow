5122d974b0906c4711ef3d69fe9c016c931c114e
Jussi Elo <jussi.elo@citrus.fi>
2020-11-05 15:43:56 +0200

    Make Gulp workflow able to handle multiple themes at the same time

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -18,9 +18,30 @@ var watch = require('gulp-watch');
 
 const watchSass = require("gulp-watch-sass");
 
-var config = require('./gulp-config.json');
 var runTimestamp = Math.round(Date.now() / 1000);
 
+const paths = {
+  scss: '/scss/**/*.scss',
+  css: '/css/',
+  generated: '/scss/generated/',
+  img: '/img/',
+  fonts: '/fonts'
+};
+
+// Default to watching all of the themes found under themes directory.
+var theme_paths = glob.sync('web/themes/custom/*');
+var themes = [];
+theme_paths.forEach(function(path) {
+  themes.push({
+    name : path.split('/').splice(-1),
+    path : path
+  });
+});
+
+/**
+ * Old maybe unnecessary code.
+ */
+var config = require('./gulp-config.json');
 const THEME_ROOT = config.theme_root;
 
 // Check if iconfont is given a special name in config file.
@@ -29,28 +50,118 @@ var font_name = config.iconfont_name;
 if (typeof font_name == 'undefined') {
   font_name = 'icons';
 }
+/**
+ * Old maybe unnecessary code ends here.
+ */
 
-const paths = {
-  styles: {
-    scss: `${THEME_ROOT}scss/`,
-    css: `${THEME_ROOT}css/`
-  },
-  generated: `${THEME_ROOT}scss/generated/`,
-  img: `${THEME_ROOT}img/`
-};
+// Create custom functions for each theme.
+themes.forEach(function(theme) {
 
-// A gulp watcher function.
+  // Create a theme specific SASS compiler.
+  gulp.task(theme.name + '_styles', function() {
+    return gulp.src([theme.path + paths.scss])
+    .pipe(plumber({errorHandler: function(err) {
+      notify.onError({
+        title: "Gulp error in " + err.plugin,
+        message: err.toString()
+      })(err);
+      this.emit('end');
+    }}))
+    .pipe(sassGlob())
+    .pipe(sass())
+    .pipe(autoprefixer({
+      cascade: false,
+    }))
+    .pipe(gulp.dest(theme.path + paths.css))
+    // Continue with minifying newly created css files.
+    .pipe(cleanCSS())
+    .pipe(gulp.dest(theme.path + paths.css));
+  });
+
+  // Create a theme specific iconfont compiler.
+  gulp.task(theme.name + '_iconfont', function(done) {
+    // This is a hack for fixing unicode prepend problems.
+    // The newer version of gulp-iconfont doesn't understand
+    // which unicode characters are already used.
+    var lastUnicode = 0xEA01; //59905
+
+    // Create config object from presets.
+    var iconfontConfig = Object.create({
+      fontHeight: 1001,
+      fontName: font_name,
+      prependUnicode: true,
+      formats: ['svg', 'ttf', 'eot', 'woff', 'woff2'],
+      normalize: true,
+      timestamp: runTimestamp,
+    });
+
+    // Read source directory and sort by name.
+    var files = fs.readdirSync(theme.path + paths.img + 'icons');
+
+    // Filter files with containing unicode value and set last unicode.
+    files.forEach(function(file) {
+      var basename = path.basename(file);
+      var matches = basename.match(/^(?:((?:u[0-9a-f]{4,6},?)+)\-)?(.+)\.svg$/i);
+      var currentCode = -1;
+
+      if(matches && matches[1]) {
+        currentCode = parseInt(matches[1].split('u')[1], 16);
+      }
+
+      if (currentCode >= lastUnicode) {
+        lastUnicode = ++currentCode;
+      }
+    });
+
+    // Set startUnicode option to determined unicode from filenames
+    iconfontConfig.startUnicode = lastUnicode;
+    // End of hack.
+
+    // Create an iconstream.
+    var iconStream = gulp.src(theme.path + paths.img + 'icons/**/*.svg')
+      .pipe(iconfont(iconfontConfig));
+
+    async.parallel([
+      function handleGlyphs(callback) {
+        iconStream.on('glyphs', function(glyphs, options) {
+          gulp.src(theme.path + paths.scss + '_icon-template.scss')
+            .pipe(consolidate('lodash', {
+              glyphs: glyphs,
+              fontName: font_name,
+              fontPath: '../fonts/',
+              className: 's'
+            }))
+            .pipe(rename('_icons.scss'))
+            .pipe(gulp.dest(theme.path + paths.generated))
+            .on('finish', callback);
+        });
+      },
+      function handleFonts(callback) {
+        iconStream
+          .pipe(gulp.dest(theme.path + paths.fonts))
+          .on('finish', callback);
+      }
+    ], done);
+  });
+});
+
+// One watcher to watch them all.
 gulp.task('watch', function() {
-  // Watch SASS files.
-  gulp.watch(paths.styles.scss + '**/*.scss', gulp.series('styles'));
-  // Watch for new or changed icons.
-  gulp.watch(paths.img + 'icons/**/*.svg', gulp.series('icons'));
+  // Loop through theme objects and watch theme specific tasks.
+  themes.forEach(function(theme) {
+    console.log('\x1b[32m%s\x1b[0m', 'Watching theme ' + theme.name + '.');
+
+    // Watch for changed SASS files.
+    gulp.watch(theme.path + paths.scss, gulp.series(theme.name + '_styles'));
+    // Watch for new or changed icons.
+    gulp.watch(theme.path + paths.img + 'icons/**/*.svg', gulp.series(theme.name + '_iconfont'));
+  });
 });
 
 // Compile SCSS to CSS. Handle errors with plumber function.
 gulp.task('styles', function() {
-  return gulp.src([paths.styles.scss + '**/*.scss'])
-  .pipe(plumber({ errorHandler: function(err) {
+  return gulp.src([theme + paths.scss])
+  .pipe(plumber({errorHandler: function(err) {
     notify.onError({
       title: "Gulp error in " + err.plugin,
       message: err.toString()
@@ -62,10 +173,10 @@ gulp.task('styles', function() {
   .pipe(autoprefixer({
     cascade: false,
   }))
-  .pipe(gulp.dest(paths.styles.css))
+  .pipe(gulp.dest(theme + paths.css))
   // Continue with minifying newly created css files.
   .pipe(cleanCSS())
-  .pipe(gulp.dest(paths.styles.css));
+  .pipe(gulp.dest(theme + paths.css));
 });
 
 // Generate icon font and a SCSS file.
@@ -114,7 +225,7 @@ gulp.task('icons', function(done) {
   async.parallel([
     function handleGlyphs(callback) {
       iconStream.on('glyphs', function(glyphs, options) {
-        gulp.src(paths.styles.scss + '_icon-template.scss')
+        gulp.src(paths.scss + '_icon-template.scss')
           .pipe(consolidate('lodash', {
             glyphs: glyphs,
             fontName: font_name,
@@ -128,7 +239,7 @@ gulp.task('icons', function(done) {
     },
     function handleFonts(callback) {
       iconStream
-        .pipe(gulp.dest(THEME_ROOT + 'fonts/'))
+        .pipe(gulp.dest(THEME_ROOT + paths.fonts))
         .on('finish', callback);
     }
   ], done);
